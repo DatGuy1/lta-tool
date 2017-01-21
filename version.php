2c96d132bb8a2038d2da94caa73a7e980c754db2
Samuel Tarling <samtarling@users.noreply.github.com>
2017-01-21 17:51:17 +0000

    Inc commit to correct

--- version.php
+++ version.php
@@ -1,5 +1,5 @@
 <?php
 //Increment the below value on each commit
-$ver_commit = "1";
+$ver_commit = "7";
 //Increment the below value on each version change (see http://semver.org/spec/v2.0.0.html)
 $ver_version = "0.8.0";
