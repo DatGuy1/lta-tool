8676292eedd850b638e4222a434c6a249de0a069
Samuel Tarling <samtarling@users.noreply.github.com>
2017-01-21 17:47:21 +0000

    Create version.php

---
+++ version.php
@@ -0,0 +1,5 @@
+<?php
+//Increment the below value on each commit
+$ver_commit = "1";
+//Increment the below value on each version change (see http://semver.org/spec/v2.0.0.html)
+$ver_version = "0.8.0";
