fc9e0340bac801bcb30c2c4c2fe0cffe8660e99a
DatGuy1 <datguysteam@gmail.com>
2017-01-23 19:36:47 +0100

    move upwards

--- request/postrequest.php
+++ request/postrequest.php
@@ -10,17 +10,17 @@ $diff = mysqli_real_escape_string($db,$_POST['confirmdiff']);
 $email = mysqli_real_escape_string($db,$_POST['email']);
 $pass = mysqli_real_escape_string($db,$_POST['password']);
 
+//fail authentication for any password exceeding 4096 bytes
+if ( strlen($pass) > 4097 ) {
+  echo "Your password exceeds 4096 bytes. Please choose another password.";
+  break;
+}
 //generate long salt
 $salt = openssl_random_pseudo_bytes(20);
 //prepend to password
 $prepared = $salt.$pass;
 //hash prepared
 $hashpass = md5($prepared);
-//fail authentication for any password exceeding 4096 bytes
-if ( strlen($pass) > 4097 ) {
-  echo "Your password exceeds 4096 bytes. Please choose another password.";
-  break;
-}
 
 $sql_insert = "INSERT INTO accrequests (requn, reqsul, reqdiff, reqemail, reqpass, salt, status) VALUES ('$username', '$sul', '$diff', '$email', '$hashpass', '$salt', '0')";
 mysqli_query($db,$sql_insert);
