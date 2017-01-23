e755c91481328ec93da97dd3c02c1d2d1a09a966
DatGuy1 <datguysteam@gmail.com>
2017-01-23 18:59:19 +0100

    Update postrequest.php

--- request/postrequest.php
+++ request/postrequest.php
@@ -3,6 +3,7 @@ session_start();
 
 
 include("../config.php");
+
 $username = mysqli_real_escape_string($db,$_POST['username']);
 $sul = mysqli_real_escape_string($db,$_POST['sul']);
 $diff = mysqli_real_escape_string($db,$_POST['confirmdiff']);
@@ -15,9 +16,11 @@ $salt = openssl_random_pseudo_bytes(20);
 $prepared = $salt.$pass;
 //hash prepared
 $hashpass = md5($prepared);
-
-
-
+//fail authentication for any password exceeding 4096 bytes
+if ( strlen($pass) > 4097 ) {
+  echo "Your password exceeds 4096 bytes. Please choose another password.";
+  break;
+}
 
 $sql_insert = "INSERT INTO accrequests (requn, reqsul, reqdiff, reqemail, reqpass, salt, status) VALUES ('$username', '$sul', '$diff', '$email', '$hashpass', '$salt', '0')";
 mysqli_query($db,$sql_insert);
